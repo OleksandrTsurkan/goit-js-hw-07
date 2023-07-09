18d77d84ce37ef64a9af356a5793c75f4efe7458
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-09 15:22:13 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -11,5 +11,19 @@ const lightbox = new SimpleLightbox(".gallery__link", {
   captionDelay: 250,
 });
 
+function createGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `;
+    })
+    .join("");
+}
+
 
 console.log(galleryItems);
