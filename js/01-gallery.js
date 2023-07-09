9b43d1c63e4dc82e9a1aad5e2727599275956f2f
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-09 14:54:35 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+let instance;
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
 
-console.log(galleryItems);
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
+function closeModal(event) {
+  if (event.code === "Escape") {
+    document.removeEventListener("keyup", closeModal);
+    instance.close();
+  }
+}
+
+function openOriginalSize(event) {
+  document.addEventListener("keyup", closeModal);
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  instance = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`
+  );
+  instance.show();
+}
+
+galleryList.addEventListener("click", openOriginalSize);
+
