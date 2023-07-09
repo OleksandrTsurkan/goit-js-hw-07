303797b9c11dea460016a67da74d7e33e0aa70d0
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-09 15:20:42 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGallaryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
 console.log(galleryItems);
